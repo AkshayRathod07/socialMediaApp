d516fdde6c41446a8e9465cdaea38f636ce0524d
Akshay <rathodak315@gmail.com>
2022-05-12 19:03:14 +0530

    small change

--- backend/app.js
+++ backend/app.js
@@ -8,17 +8,17 @@ if (process.env.NODE_ENV !== "production") {
 
 
   // Using Middlewares
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+// app.use(express.json({ limit: "50mb" }));
+// app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 
-  // Importing Routes
-const post = require("./routes/post");
-const user = require("./routes/user");
+//   // Importing Routes
+// const post = require("./routes/post");
+// const user = require("./routes/user");
 
-// Using Routes
-app.use("/api/v1", post);
-app.use("/api/v1", user);
+// // Using Routes
+// app.use("/api/v1", post);
+// app.use("/api/v1", user);
   
 
 module.exports = app;
